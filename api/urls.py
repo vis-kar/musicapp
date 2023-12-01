2bd143ac73a54c81eac6d915cb3698dafac9fe5d
vishwa <vishwa@thisisdebian.in>
2023-12-01 12:26:39 +0530

    Second commit

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('rooms', views.RoomView.as_view()),
+    path('create-room', views.CreateRoomView.as_view()),
+    path('get-room', views.GetRoom.as_view())
+]
---
+++ api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from . import serializers
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = serializers.RoomSerializer
+
+class GetRoom(APIView):
+    serializer_class = serializers.RoomSerializer
+    lookup_url_kwarg = 'code'
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room)>0:
+                data = serializers.RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room code'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Code parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateRoomView(APIView):
+    serializer_class = serializers.CreateRoomSerializer
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data['votes_to_skip']
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+            
+            return Response(serializers.RoomSerializer(room).data, status=status.HTTP_201_CREATED)
