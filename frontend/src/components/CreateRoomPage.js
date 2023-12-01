ca565d8f6e2aae747dbb7557449647cee0353489
vishwa <vishwa@thisisdebian.in>
2023-12-02 00:24:53 +0530

    Made aesthetic changes

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,100 +1,84 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Grid, Button, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel, FormLabel } from "@material-ui/core"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
+export default function CreateRoomPage(props) {
+  const defaultVotes = 2;
+  const [guestCanPause, setGuestCanPause] = useState(false);
+  const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
+  const navigate = useNavigate();
 
-export default class CreateRoomPage extends Component {
-  defaultVotes = 2;
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      guestCanPause: true,
-      votesToSkip: this.defaultVotes
-    }
-    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this)
-    this.handleVotesChanged = this.handleVotesChanged.bind(this)
+  const handleVotesChanged = e => {
+    setVotesToSkip(e.target.value)
   }
-
-  handleVotesChanged(e) {
-    this.setState({
-      votesToSkip: parseInt(e.target.value),
-    })
+  const handleGuestCanPauseChange = e => {
+    setGuestCanPause(e.target.value)
   }
-
-  handleGuestCanPauseChange(e) {
-    this.setState({
-      guestCanPause: e.target.value === 'true' ? true : false,
-    })
-  }
-
-  handleCreateRoom = () => {
+  const handleCreateRoom = async e => {
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        votes_to_skip: this.state.votesToSkip,
-        guest_can_pause: this.state.guestCanPause
+        votes_to_skip: votesToSkip,
+        guest_can_pause: guestCanPause
       })
     }
-    fetch('/api/create-room', requestOptions)
+    await fetch('/api/create-room', requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => navigate('/room/'+data.code));
   }
 
-  render() {
-    return (
-      <Grid container spacing={1}>
-        <Grid item xs={12} align="center">
-          <Typography component='h4' variant="h4">
-            Create A Room
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <FormControl component='fieldset'>
-            <FormHelperText>
-              <div align='center'>Guest control of Playback State</div>
-            </FormHelperText>
-            <RadioGroup row defaultValue='true' onChange={this.handleGuestCanPauseChange}>
-              <FormControlLabel
-                value='true'
-                control={<Radio color="primary" />}
-                label='Play/Pause'
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value='false'
-                control={<Radio color="secondary" />}
-                label='No Control'
-                labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} align='center'>
-          <FormControl>
-            <TextField required={true} type="number" defaultValue={this.defaultVotes} inputProps={{
-              min: 1,
-              style: {
-                textAlign: 'center'
-              }
-            }}
-              onChange={this.handleVotesChanged}
-            />
-            <FormHelperText>
-              <div align='center'>Votes Required to Skip Song</div>
-            </FormHelperText>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} align='center'>
-          <Button color="primary" variant="contained" onClick={this.handleCreateRoom}>Create A Room</Button>
-        </Grid>
-        <Grid item xs={12} align='center'>
-          <Button color="secondary" variant="contained" to='/' component={Link}>Back</Button>
-        </Grid>
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component='h4' variant="h4">
+          Create A Room
+        </Typography>
       </Grid>
-    )
-  }
+      <Grid item xs={12} align="center">
+        <FormControl component='fieldset'>
+          <FormHelperText>
+            <div align='center'>Guest control of Playback State</div>
+          </FormHelperText>
+          <RadioGroup row defaultValue='true' onChange={handleGuestCanPauseChange}>
+            <FormControlLabel
+              value='true'
+              control={<Radio color="primary" />}
+              label='Play/Pause'
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value='false'
+              control={<Radio color="secondary" />}
+              label='No Control'
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <FormControl>
+          <TextField required={true} type="number" defaultValue={defaultVotes} inputProps={{
+            min: 1,
+            style: {
+              textAlign: 'center'
+            }
+          }}
+            onChange={handleVotesChanged}
+          />
+          <FormHelperText>
+            <div align='center'>Votes Required to Skip Song</div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <Button color="primary" variant="contained" onClick={handleCreateRoom}>Create A Room</Button>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <Button color="secondary" variant="contained" to='/' component={Link}>Back</Button>
+      </Grid>
+    </Grid>
+  )
 }
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,73 +1,91 @@
-import React, {Component} from "react";
+import React from "react";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export default class RoomJoinPage extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-          roomCode: '',
-          error: ''
-        }
-    }
+export default function RoomJoinPage() {
+  const [roomCode, setRoomCode] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    render() {
-      return (
-        <Grid container spacing={1} >
-          <Grid item xs={12} align='center'>
-            <Typography variant="h4" component='h4'>
-              Join a Room
-            </Typography>
-          </Grid>
-          <Grid item xs={12} align='center'>
-            <TextField
-              error={this.state.error}
-              label='Code'
-              placeholder='Enter a Room Code'
-              value={this.state.roomCode}
-              helperText={this.state.error}
-              variant='outlined'
-              onChange = {this.handleTextFieldChange}
-            />
-          </Grid>
-          <Grid item xs={12} align='center'>
-            <Button variant="contained" color="primary" component={Link} onClick={this.roomButtonPressed}>Enter Room</Button>
-          </Grid>
-          <Grid item xs={12} align='center'>
-            
-          </Grid>
-          <Grid item xs={12} align='center'>
-            <Button variant="contained" color="secondary" to='/' component={Link}>Back</Button>
-          </Grid>
-        </Grid>
-      )
-    }
+  const handleTextFieldChange = (event) => {
+    setRoomCode(event.target.value);
+  };
 
-    handleTextFieldChange = e => {
-      this.setState({
-        roomCode: e.target.value
-      })
-    }
-
-    roomButtonPressed = () => {
-      const requestOptions = {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
+  const roomButtonPressed = async () => {
+    try {
+      const response = await fetch('/api/join-room', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify({
-          code: this.state.roomCode 
-        })
-      }
+          code: roomCode,
+        }),
+      });
 
-      fetch('/api/join-room', requestOptions).then(response=>{
-        if(response.ok) {
-          
-        } else {
-          this.setState({
-            error: 'Room not found'
-          })
-        }
-      }).catch(error=>{
-        console.log(error)
-      })
+      if (!response.ok) {
+        throw new Error('Room not found');
+      }
+      setError('')
+      navigate('/room/'+roomCode)
+      // Handle successful join here
+    } catch (err) {
+      setError(err.message);
     }
+  };
+
+  return (
+    <Grid
+      container
+      spacing={1}>
+      <Grid
+        item
+        xs={12}
+        align="center">
+        <Typography
+          variant="h4"
+          component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        align="center">
+        <TextField
+          error={error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        align="center">
+        <Button
+          variant="contained"
+          color="primary" component={Link} onClick={roomButtonPressed}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center"></Grid>
+      <Grid
+        item
+        xs={12}
+        align="center">
+        <Button
+          variant="contained"
+          color="secondary"
+          to="/"
+          component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
